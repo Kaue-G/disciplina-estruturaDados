815e23f00a5a2fbe6f8be77fac37360ba7fdf05f
Kaue-G <kaueguedes.dev@gmail.com>
2022-05-11 12:27:22 -0300

    iniciando implementação do remover folha

--- src/br/com/ifce/estrutura/arvore/ArvoreBinario.java
+++ src/br/com/ifce/estrutura/arvore/ArvoreBinario.java
@@ -17,20 +17,44 @@ public class ArvoreBinario {
 		}
 	}
 
-	public void addNo(String Add, String posicao, NoBinario noPai) {
+	public NoBinario addNo(String Add, String posicao, NoBinario noPai) {
 		NoBinario noToAdd = new NoBinario(Add);
 		if (noPai == null) {
 			addFilho(noToAdd, posicao, root);
 		} else {
 			addFilho(noToAdd, posicao, noPai);
 		}
+		
+		return noToAdd;
 	}
 
 	private void addFilho(NoBinario noToAdd, String posicao, NoBinario pai) {
+		noToAdd.setPai(pai);
+		
 		if (posicao.equals("direita")) {
 			pai.setDireita(noToAdd);
 		} else {
 			pai.setEsquerda(noToAdd);
+		}
+	}
+	
+	public void removeNo(String posicao, NoBinario pai) {
+		if (pai.getEsquerda() == null) {
+			System.out.println("não ha nó a ser removido");
+		} else {
+			if (pai.getDireita() == null) {
+				System.out.println("não ha nó a ser removido");
+			} else {
+				removeFilho(posicao, pai);
+			}
+		}
+	}
+	
+	private void removeFilho(String posicao, NoBinario pai) {
+		if (posicao.equals("direita")) {
+			pai.setDireita(null);
+		} else {
+			pai.setEsquerda(null);
 		}
 	}
 }
--- src/br/com/ifce/estrutura/arvore/HelloArvore.java
+++ src/br/com/ifce/estrutura/arvore/HelloArvore.java
@@ -6,7 +6,11 @@ public class HelloArvore {
 		
 		ArvoreBinario arvoreBinario = new ArvoreBinario();
 		arvoreBinario.addNo("folha1", "esquerda", null);
-		arvoreBinario.addNo("folha2", "direita", null);
+		NoBinario folha2 = arvoreBinario.addNo("folha2", "direita", null);
 		
+		NoBinario testeRemove1 = arvoreBinario.addNo("folha3", "esquerda", folha2);		
+		NoBinario testeRemove2 = arvoreBinario.addNo("folha4", "direita", folha2);
+		
+		arvoreBinario.removeNo("esquerda", folha2);
 	}
 }
